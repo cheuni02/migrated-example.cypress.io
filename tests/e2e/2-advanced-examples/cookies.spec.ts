41f0927dd19ea6b3b0dc36d50d1928edb560746a
cheuni02 <inhcheung@gmail.com>
2025-12-24 16:50:00 +0000

    added 2 cookies page tests

--- tests/e2e/2-advanced-examples/cookies.spec.ts
+++ tests/e2e/2-advanced-examples/cookies.spec.ts
@@ -60,4 +60,87 @@ test.describe('cookies', () => {
       expect(cookies[0]).toHaveProperty('domain');
       expect(cookies[0]).toHaveProperty('path');
    });
+
+   test('cy.getAllCookies() - get all browser cookies', async () => {
+      /*
+      it("cy.getAllCookies() - get all browser cookies", () => {
+      // https://on.cypress.io/getallcookies
+      cy.getAllCookies().should("be.empty");
+
+      cy.setCookie("key", "value");
+      cy.setCookie("key", "value", { domain: ".example.com" });
+
+      // cy.getAllCookies() yields an array of cookies
+      cy.getAllCookies()
+      .should("have.length", 2)
+      .should((cookies) => {
+        // each cookie has these properties
+        expect(cookies[0]).to.have.property("name", "key");
+        expect(cookies[0]).to.have.property("value", "value");
+        expect(cookies[0]).to.have.property("httpOnly", false);
+        expect(cookies[0]).to.have.property("secure", false);
+        expect(cookies[0]).to.have.property("domain");
+        expect(cookies[0]).to.have.property("path");
+
+        expect(cookies[1]).to.have.property("name", "key");
+        expect(cookies[1]).to.have.property("value", "value");
+        expect(cookies[1]).to.have.property("httpOnly", false);
+        expect(cookies[1]).to.have.property("secure", false);
+        expect(cookies[1]).to.have.property("domain", ".example.com");
+        expect(cookies[1]).to.have.property("path");
+      });
+      });
+
+      */
+      cookies = await cookiesPage.getCookies();
+      expect(cookies).toEqual([]);
+
+      await cookiesPage.addCookies([
+         { name: 'key', value: 'value', domain: '.example.cypress.io', path: '/'},
+         { name: 'key', value: 'value', domain: '.example.com', path: '/'}
+      ]);
+
+      cookies = await cookiesPage.getCookies();
+      expect(cookies.length).toEqual(2);
+      expect(cookies[0]).toHaveProperty('name', 'key');
+      expect(cookies[0]).toHaveProperty('value', 'value');
+      expect(cookies[0]).toHaveProperty('httpOnly', false);
+      expect(cookies[0]).toHaveProperty('secure', false);
+      expect(cookies[0]).toHaveProperty('domain');
+      expect(cookies[0]).toHaveProperty('path');
+
+      expect(cookies[1]).toHaveProperty('name', 'key');
+      expect(cookies[1]).toHaveProperty('value', 'value');
+      expect(cookies[1]).toHaveProperty('httpOnly', false);
+      expect(cookies[1]).toHaveProperty('secure', false);
+      expect(cookies[1]).toHaveProperty('domain', '.example.com');
+      expect(cookies[1]).toHaveProperty('path');
+   });
+
+   test('cy.setCookie() - set a browser cookie', async () => {
+      /*
+        it("cy.clearCookie() - clear a browser cookie", () => {
+      // https://on.cypress.io/clearcookie
+      cy.getCookie("token").should("be.null");
+
+      cy.get("#clearCookie .set-a-cookie").click();
+
+      cy.getCookie("token").should("have.property", "value", "123ABC");
+
+      // cy.clearCookies() yields null
+      cy.clearCookie("token");
+
+      cy.getCookie("token").should("be.null");
+      });
+       */
+      cookies = await cookiesPage.getCookies();
+      expect(cookies).toEqual([]);
+      await cookiesPage.addCookies([
+         { name: 'foo', value: 'bar', domain: '.example.cypress.io', path: '/'}
+      ]);
+      cookies = await cookiesPage.getCookies();
+
+      expect(cookies.find(c => c.name === 'foo')?.value)?.toEqual('bar');
+   })
+
 });
